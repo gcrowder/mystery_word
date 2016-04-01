92da65a7a5bdbecf1c079e67490590d00f222a71
gcrowder <gregory.j.crowder@gmail.com>
2016-03-31 22:24:47 -0400

    Renamed file_open to get_words. Wrote make_appropriate_difficulty to get list of appropriately difficult words. Wrote unit tests for make_appropriate difficulty. Wrote get_difficulty to get the difficulty level from the user. Don't know how to write a unit test for input().

--- mystery_word.py
+++ mystery_word.py
@@ -1,10 +1,48 @@
 import random
 
+def greeting():
+    print("Hello! Shall we play a word game?")
+    print("I will pick a word and you will try to guess it one letter at a time.")
 
-def open_file(path='/usr/share/dict/words'):
+def get_difficulty():
+    while True:
+        difficulty = input("How difficult should this game be? Would you like [E]asy, [N]ormal, or [H]ard? ").lower()
+        if difficulty == 'e' or difficulty == 'easy':
+            return 'EASY'
+        elif difficulty == 'n' or difficulty == 'normal':
+            return 'NORMAL'
+        elif difficulty == 'h' or difficulty == 'hard':
+            return 'HARD'
+        else:
+            print("What was that? I didn't quite understand you. Let's try again.")
+
+
+#get words file
+def get_words(path='/usr/share/dict/words'):
     list_of_words = []
     with open(path, 'r') as words:
         for line in words:
             word = line.strip()
             list_of_words.append(word)
     return list_of_words
+
+#difficulty level: easy (4-6 character words), normal(6-8 character words), hard (8+ character words)
+def make_appropriate_difficulty(word_list, difficulty='EASY'):
+    dictionary_of_pain = {'EASY': (3,7), 'NORMAL': (5,9), 'HARD': (7,25)}
+    appropriate_difficulty = []
+    for word in word_list:
+            if len(word) > dictionary_of_pain[difficulty][0] and len(word) < dictionary_of_pain[difficulty][1]:
+                appropriate_difficulty.append(word)
+    return appropriate_difficulty
+
+
+def main():
+    all_the_words = get_words()
+    #get difficulty from user
+    #choose random word from list of appropriate_difficulty words
+    mystery_word = random.choice(make_appropriate_difficulty(all_the_words, difficulty))
+
+
+#tell/show user the length of the mystery_word
+
+#user guesses one letter per round. one letter input only, must be a character.
--- mystery_word_test.py
+++ mystery_word_test.py
@@ -1,16 +1,35 @@
 import unittest
 import mystery_word
 
+test_words = ['i', 'spoke', 'to', 'several', 'people', 'with', 'delayed', 'sleep', 'phase', 'a', 'condition', 'that', 'congressional']
+
+
+
 class TestMysteryWord(unittest.TestCase):
 
-    def test_assert_equal(self):
-        self.assertEqual(mystery_word.open_file('test.txt'), ['house', 'mouse', 'louse', 'douse', 'souse'])
+    def test_assert_equal_file(self):
+        self.assertEqual(mystery_word.get_words('test.txt'), ['house', 'mouse', 'louse', 'douse', 'souse'])
 
     def test_assert_not_equal(self):
-        self.assertNotEqual(mystery_word.open_file(), ['house', 'mouse', 'louse', 'douse', 'souse'])
+        self.assertNotEqual(mystery_word.get_words(), ['house', 'mouse', 'louse', 'douse', 'souse'])
 
+    def test_assert_equal_difficulty_1(self):
+        self.assertEqual(mystery_word.make_appropriate_difficulty(test_words, 'EASY'), ['spoke', 'people', 'with', 'sleep', 'phase', 'that'])
 
+    def test_assert_equal_difficulty_2(self):
+        self.assertEqual(mystery_word.make_appropriate_difficulty(test_words, 'NORMAL'), ['several', 'people', 'delayed'])
 
+    def test_assert_equal_difficulty_3(self):
+        self.assertEqual(mystery_word.make_appropriate_difficulty(test_words, 'HARD'), ['condition', 'congressional'])
+
+    def test_assert_not_equal_difficulty_1(self):
+        self.assertNotEqual(mystery_word.make_appropriate_difficulty(test_words, 'EASY'), ['several', 'people', 'delayed'])
+
+    def test_assert_not_equal_difficulty_2(self):
+        self.assertNotEqual(mystery_word.make_appropriate_difficulty(test_words, 'NORMAL'), ['spoke', 'people', 'with', 'sleep', 'phase', 'that'])
+
+    def test_assert_not_equal_difficulty_3(self):
+        self.assertNotEqual(mystery_word.make_appropriate_difficulty(test_words, 'HARD'), ['spoke', 'people', 'with', 'sleep', 'phase', 'that'])
 
 if __name__ == '__main__':
     unittest.main()
